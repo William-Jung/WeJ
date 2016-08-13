fc7678234954eeadec47b9184399c52446682685
William Jung & Helom Berhane <pair+William-Jung.helomberhane@devbootcamp.com>
2016-08-13 16:30:08 -0500

    Fix association to grab specific songs from playlist

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@ Playlist.create!(name: "Adele", admin: helom, passcode: 222222, request_limit: 5
 Playlist.create!(name: "DMX", admin: dumitru, passcode: 333333, request_limit: 4, flag_minimum: 4, allow_explicit: true)
 Playlist.create!(name: "Down with the Sickness", admin: will, passcode: 666666, request_limit: 6, flag_minimum: 6, allow_explicit: true)
 Playlist.create!(name: "Celine's Greatest Hits", admin: helom, passcode: 7777777, request_limit: 7, flag_minimum: 1, allow_explicit: true)
+
+25.times do
+  Song.create(title: Faker::Hacker.noun, artist: Faker::Name.name, album: Faker::Hacker.noun, release_date: Date.today, album_art: nil)
+end
+
+50.times do
+  Playlistsong.create(playlist_id: rand(1..5), song_id: rand(1..25))
+end
