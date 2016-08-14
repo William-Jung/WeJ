ec0345c814db3bc0550dccc9aa1a745823ca391b
William Jung & Dumitru Gutu <pair+William-Jung.dumitrugutu@devbootcamp.com>
2016-08-14 14:17:13 -0500

    Redirect back to the playlist after voting on a song

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,10 @@
 class VotesController < ApplicationController
 
   def create
-    vote = Vote.create(user_id: params[:user_id], playlistsong: Playlistsong.find(params[:id]), request_type: params[:request_type])
-    p "+++++++++++++++++++++++++++"
-    p vote
-    p "+++++++++++++++++++++++++++"
+    playlistsong = Playlistsong.find(params[:id])
+    Vote.create(user_id: params[:user_id], playlistsong: playlistsong, request_type: params[:request_type])
+    playlist = Playlist.find(playlistsong.playlist_id)
+    redirect_to show_playlist_path(playlist)
   end
 
 end
