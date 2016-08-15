48bb9674e0526bc8ce88be0ab4642799e022b919
William Jung & Max Greschl <pair+William-Jung.mgreschl@devbootcamp.com>
2016-08-15 18:46:13 -0500

    add update_playlist_rankings method to playlist model

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,11 @@
 class VotesController < ApplicationController
-
+  include UsersHelper
   def create
     if logged_in?
       playlistsong = Playlistsong.find(params[:id])
       Vote.create(user_id: params[:user_id], playlistsong: playlistsong, request_type: params[:request_type])
       playlist = Playlist.find(playlistsong.playlist_id)
+      playlist.update_playlist_rankings
       redirect_to show_playlist_path(playlist)
     else
       redirect_to new_session_path
