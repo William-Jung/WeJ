dd4c055299274271c80f60cd0ed3963ffdd7841c
Dumitru Gutu & Laura Rumage <pair+dumitrugutu.laurarumage@devbootcamp.com>
2016-08-15 15:57:49 -0500

    working with error messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path
     else
+      @message = "Incorrect email or password"
       render "new"
     end
   end
