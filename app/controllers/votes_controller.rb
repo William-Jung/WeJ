3cd13ffab567a475df820619b3859b94b91b1085
Helom Berhane <email": null>
2016-08-17 16:34:09 -0500

    added auth

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,10 +4,10 @@ class VotesController < ApplicationController
     if logged_in?
       playlistsong = Playlistsong.find(params[:id])
       playlist = Playlist.find(playlistsong.playlist_id)
-      if playlist.votes.where(user_id: current_user.id, request_type: 'vote').count < playlist.request_limit
+      # if playlist.votes.where(user_id: current_user.id, request_type: 'vote').count < playlist.request_limit
         Vote.create(user_id: params[:user_id], playlistsong: playlistsong, request_type: params[:request_type])
         playlist.update_playlist_rankings
-      end
+      # end
       redirect_to show_playlist_path(playlist)
     else
       redirect_to new_session_path
