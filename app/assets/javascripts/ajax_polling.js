d70e48811a341bd4008b92e0943451f637e609e6
William Jung <email": null>
2016-08-18 11:28:20 -0500

    add html and css to make search bar stick to top of page and update responsiveness for different size screens

--- app/assets/javascripts/ajax_polling.js
+++ app/assets/javascripts/ajax_polling.js
@@ -1,13 +1,13 @@
-setInterval(function(){
-  var windowUrl = window.location.href;
-  var pattern = new RegExp(/playlists\/\d+$/);
-  if (pattern.test(location.pathname)) {
-    $.ajax({
-      method: 'get',
-      url: windowUrl
-    }).done(function(response){
-      $('#song-list').html('');
-      $('#song-list').append(response);
-    })
-  }
-}, 1000);
+// setInterval(function(){
+//   var windowUrl = window.location.href;
+//   var pattern = new RegExp(/playlists\/\d+$/);
+//   if (pattern.test(location.pathname)) {
+//     $.ajax({
+//       method: 'get',
+//       url: windowUrl
+//     }).done(function(response){
+//       $('#song-list').html('');
+//       $('#song-list').append(response);
+//     })
+//   }
+// }, 1000);
