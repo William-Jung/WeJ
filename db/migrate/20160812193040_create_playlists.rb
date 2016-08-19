5832f65d8d3dece36d9c67aa04d72693bb348494
Helom Berhane <email": null>
2016-08-19 11:36:52 -0500

    fix heroku

--- db/migrate/20160812193040_create_playlists.rb
+++ db/migrate/20160812193040_create_playlists.rb
@@ -1,12 +1,7 @@
 class CreatePlaylists < ActiveRecord::Migration
   def change
     create_table :playlists do |t|
-      t.string :name
-      t.integer :admin_id
-      t.string :passcode
-      t.integer :request_limit
-      t.integer :flag_minimum
-      t.boolean :allow_explicit
+
 
       t.timestamps null: false
     end
