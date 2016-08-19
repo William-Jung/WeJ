5735404e2b5a1713b15e3edf049545c33bda4930
William Jung <email": null>
2016-08-19 10:46:14 -0500

    change assets to files

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Wej
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.serve_static_assets = true
+    config.serve_static_files = true
   end
 end
 
